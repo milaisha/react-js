3efc6aa7fa796a087f2db52a7e4369f7f558b5f7
Aisha <milaisha098@gmail.com>
2023-08-10 21:33:52 +0300

    add new file

---
+++ script.js
@@ -0,0 +1,60 @@
+const accessKey = "bDLcQjt_rnoFlxDJghk5swLCMNp2aXhYu6q4kw_XoJ8";
+   
+const formEl = document.querySelector("form");
+const inputEl =  document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+let inputData ="";
+let page = 1;
+
+ async function searchImages(){
+    inputData = inputEl.value;
+    const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}';
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results;
+
+
+    if(page === 1){
+        searchResults.innerHTML = "";
+    }
+
+    results.map((result) => {
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("search-Result");
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
+
+
+        imageLink.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResults.appendChild(imageWrapper);
+
+    });
+
+    page++
+    if (page > 1){
+        showMore.style.display = "block";
+    }
+}
+  
+
+formEl.addEventListener("submit", (event) =>{
+    event.preventDefault()
+    page = 1;
+    searchImages()
+})
+
+showMore.addEventListener("click", () =>{
+
+    searchImages()
+})
+
